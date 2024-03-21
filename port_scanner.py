413c17db23c7b096b9376a746fea7ec7a9d4158e
Nic Robbins <nicmmrob@gmail.com>
2024-03-21 23:16:56 +0000

    init commit

--- port_scanner.py
+++ port_scanner.py
@@ -1,8 +1,15 @@
 import socket
 
-def get_open_ports(target, port_range, verbose = False):
+def get_open_ports(target, port_range, verbose = True):
     open_ports = []
-
-
-
+    host = socket.gethostbyname(target)
+    port = range(port_range[0], port_range[1]+1)
+    for port in port:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(5)
+        result = s.connect_ex((host, port))
+        if result == 0:
+            open_ports.append(port)
     return(open_ports)
+
+get_open_ports("209.216.230.240", [440, 445])
