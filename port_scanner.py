e8263d2c0c0d92eef1b1579fdeea4e8d07747264
Nic Robbins <nicmmrob@gmail.com>
2024-03-22 21:58:53 +0000

    init commit

--- port_scanner.py
+++ port_scanner.py
@@ -1,20 +1,21 @@
 import socket
+from common_ports import ports_and_services
+
 
 def get_open_ports(target, port_range, verbose = True):
     open_ports = []
-    host = socket.gethostbyname(target)
-    port = range(port_range[0], port_range[1]+1)
-    for port in port:
+    ip = socket.get
+    port = range(port_range[0], port_range[1])
+    if port in ports_and_services:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(5)
-        result = s.connect_ex((host, port))
-        if result == 0:
+        result = s.connect_ex((ip, port))
             open_ports.append(port)
-            def port_scanner(port):
-      if s.connect_ex((host, port)):
-        print("The port is closed")
-      else:
-        print("The ports for", host, "are", open_ports)
+        def port_scanner(port):
+            if s.connect_ex((ip, port)):
+                print("The port is closed")
+            else:
+                print("The ports for", ip, "are", open_ports)
     return(open_ports)
 
-port_scanner.get_open_ports("209.216.230.240", [440, 445])
+port_scanner(get_open_ports("209.216.230.240", [440, 445]))
