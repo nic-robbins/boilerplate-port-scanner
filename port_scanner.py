bb06208032f140340fe8434da127a4d2d0b33bcf
Nic Robbins <nicmmrob@gmail.com>
2024-03-23 01:07:16 +0000

    functional changes

--- port_scanner.py
+++ port_scanner.py
@@ -1,21 +1,28 @@
 import socket
 from common_ports import ports_and_services
 
+def get_open_ports(target, port_range, verbose: bool):
+		open_ports = []
+		host = socket.gethostbyname(target)
 
-def get_open_ports(target, port_range, verbose = True):
-    open_ports = []
-    ip = socket.get
-    port = range(port_range[0], port_range[1])
-    if port in ports_and_services:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(5)
-        result = s.connect_ex((ip, port))
-            open_ports.append(port)
-        def port_scanner(port):
-            if s.connect_ex((ip, port)):
-                print("The port is closed")
-            else:
-                print("The ports for", ip, "are", open_ports)
-    return(open_ports)
+		for port in range(port_range[0], port_range[1]):
+				s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				s.settimeout(5)
+				result = s.connect_ex((host, port))
 
-port_scanner(get_open_ports("209.216.230.240", [440, 445]))
+				if result == 0:
+						open_ports.append(port)
+						if verbose:
+								service = ports_and_services.get(port, "Unknown")
+								print(f"Open ports for {target}: ")
+								print(f"Port: {port} Service: {service}")
+						s.close()
+						if not verbose:
+							print(f"Open ports: {port}")
+						else:
+							print("No open ports found.")
+	
+		return open_ports
+
+open_ports = get_open_ports("hackthissite.org", [440, 445], True)
+
